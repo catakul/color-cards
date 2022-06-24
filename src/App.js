1ae072d8858a3b2a36cc96f1ff4ee0f34055d9f7
Ava-Victoria Crivello <Avcrivello@gmail.com>
2022-06-24 16:26:39 +0200

    firstInit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+
+const colorCards = [
+  { id: 1,
+  colorCode: '#ccc'
+
+},
+
+{
+  id: 2,
+  colorCode: '#12b886'
+}
+
+
+
+
+
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Cards name="colorCard" style={{ backgroundColor: color }}>
+        <button type="submit">Karte Speichern!</button>
+      </Cards>
+    </>
+   
   );
 }
 
